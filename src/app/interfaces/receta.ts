5ae3804e7fec9015bfa214d73d6bd24557b52c56
Alejandro González <152494270+alego128@users.noreply.github.com>
2025-11-28 10:36:29 +0100

    Delete src directory

--- src/app/interfaces/receta.ts
+++
@@ -1,9 +0,0 @@
-export interface Receta {
-  nombre: string;
-  imagen: string;
-  tiempoPreparacion: number;
-  descripcion: string;
-  categoria?: string;
-  dificultad?: string;
-  ingredientes?: string[];
-}
